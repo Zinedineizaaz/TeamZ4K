77fd64f813221be50e14a784714e77f5d5dec2b6
Khairan Aja <khairking6@gmail.com>
2025-12-24 15:47:15 +0700

    Fitur Final: Super Admin (Police), Security Fixes & Smart Redirect

--- app/Http/Middleware/IsPolice.php
+++ app/Http/Middleware/IsPolice.php
@@ -10,20 +10,19 @@ class IsPolice
 {
     /**
      * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // 1. Cek apakah user sedang login?
-        // 2. Cek apakah kolom 'role' di database isinya 'superadmin'?
+        // 1. Cek apakah user sedang login DAN apakah rolenya superadmin
         if (auth()->check() && auth()->user()->role === 'superadmin') {
             
             // Kalau IYA (Police), silakan lanjut masuk
             return $next($request);
         }
 
-        // Kalau BUKAN Police, tendang balik ke halaman home/dashboard
-        return redirect('/')->with('error', 'Akses Ditolak! Halaman ini hanya untuk Police.');
+        // --- UBAH BAGIAN INI ---
+        // Jangan redirect ke '/', tapi stop proses dan tampilkan Error 403.
+        // Laravel otomatis akan mencari file view di resources/views/errors/403.blade.php
+        abort(403, 'Akses Ditolak! Halaman ini hanya untuk Police.');
     }
 }

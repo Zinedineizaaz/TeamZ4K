cca25bc1b7a99d297bc67357a3c9fe6e22eca632
Zinedineizaaz <zndndaffa@gmail.com>
2026-01-03 01:59:09 +0700

    Fix API index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,14 @@
-<?php require __DIR__ . '/../public/index.php';
+<?php
+
+// Pindahkan folder storage dan cache ke /tmp agar writable
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
+$app->setBootstrapContainerPath('/tmp/bootstrap');
+
+// Pastikan folder yang dibutuhkan ada
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+
+require __DIR__ . '/../public/index.php';

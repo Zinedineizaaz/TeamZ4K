c1d9c0a4f9410735a8be8c64cfdb4f848d81b580
Zinedineizaaz <zndndaffa@gmail.com>
2026-01-13 11:32:23 +0700

    update certs

--- config/database.php
+++ config/database.php
@@ -59,7 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : null,
+                // base_path() akan otomatis menyesuaikan folder certs di lokal maupun Vercel
+                PDO::MYSQL_ATTR_SSL_CA => base_path('certs/isrgrootx1.pem'),
             ]) : [],
         ],
         'pgsql' => [

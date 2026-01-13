7a8e14bd7629e4e3fc97899e7d033bdb5f390ba6
Zinedineizaaz <zndndaffa@gmail.com>
2026-01-13 11:26:39 +0700

    Update config

--- config/database.php
+++ config/database.php
@@ -58,9 +58,8 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            // --- PERBAIKAN DI SINI ---
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : null,
             ]) : [],
         ],
         'pgsql' => [
